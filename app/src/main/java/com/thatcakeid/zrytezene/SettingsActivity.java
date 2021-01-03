21a9bdbdd23866214180dd0127c2a4c4f4614390
ryenyuku <teamworks1732@gmail.com>
2021-01-03 12:54:23 +0700

    Restored my local changes

--- app/src/main/java/com/thatcakeid/zrytezene/SettingsActivity.java
+++ app/src/main/java/com/thatcakeid/zrytezene/SettingsActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
