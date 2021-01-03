373bed76e42d392f4856612c5f3e6bd0f0c47cc5
ryenyuku <teamworks1732@gmail.com>
2021-01-03 16:04:33 +0700

    Some changes to implement Firestore

--- app/src/main/java/com/thatcakeid/zrytezene/MainActivity.java
+++ app/src/main/java/com/thatcakeid/zrytezene/MainActivity.java
@@ -1,14 +1,52 @@
 package com.thatcakeid.zrytezene;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore versions_db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+        versions_db = FirebaseFirestore.getInstance();
+        versions_db.collection("versions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            /* for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("tes", document.getId() + " => " + document.getData());
+                            } */
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        } else
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
