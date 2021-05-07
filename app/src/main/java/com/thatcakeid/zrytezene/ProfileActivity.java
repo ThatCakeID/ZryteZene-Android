7c897aaef7bdcf3c20df6287bd542a25f79db6a5
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-07 07:58:11 +0700

    feat: Implement ProfileActivity

--- app/src/main/java/com/thatcakeid/zrytezene/ProfileActivity.java
+++ app/src/main/java/com/thatcakeid/zrytezene/ProfileActivity.java
@@ -1,20 +1,38 @@
 package com.thatcakeid.zrytezene;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.thatcakeid.zrytezene.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private String uid;
+    ActivityProfileBinding binding;
+
+    String uid;
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    DocumentReference user_ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         Intent intent = getIntent();
         uid = intent.getStringExtra("uid");
+        user_ref = database.collection("users").document(uid);
+
+        user_ref.get()
+                .addOnSuccessListener(snapshot -> {
+                    binding.userName.setText(snapshot.getString("username"));
+                    binding.userBio.setText(snapshot.getString("description"));
+                });
     }
 }
