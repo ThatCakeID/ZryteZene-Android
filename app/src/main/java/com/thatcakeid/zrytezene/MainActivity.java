e630878a1867c0b05771c7a7070d91daed640897
ryenyuku <teamworks1732@gmail.com>
2021-09-12 18:30:40 +0700

    Implemented userdata bottomsheet

--- app/src/main/java/com/thatcakeid/zrytezene/MainActivity.java
+++ app/src/main/java/com/thatcakeid/zrytezene/MainActivity.java
@@ -5,15 +5,23 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
@@ -27,12 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this);
-        FirebaseFirestore versions_db = FirebaseFirestore.getInstance();
+        CollectionReference versions_db = FirebaseFirestore.getInstance().collection("versions");
         auth = FirebaseAuth.getInstance();
 
-        // Set collection reference to 'versions'
-        versions_db.collection("versions")
-                .get() // Fetch the data to client
+        versions_db.get() // Fetch the data to client
                 // Set a listener that listen if the data is already received
                 .addOnSuccessListener(queryDocumentSnapshots -> {
                     // Convert the result to List<DocumentSnapshot> and get the first item
@@ -64,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                                                 finish();
                                             })
                                             .addOnFailureListener(e -> {
-                                                // TODO: implement this
+                                                showBottomSheet();
                                             });
 
                                     return;
@@ -88,5 +94,34 @@ public class MainActivity extends AppCompatActivity {
                     // Show the error to user
                     Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
                 });
+    }
+
+    private void showBottomSheet() {
+        final CollectionReference user_ref = FirebaseFirestore.getInstance().collection("users");
+
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.sheet_userdata, null);
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+
+        view.findViewById(R.id.button_ok).setOnClickListener(v -> {
+            HashMap<String, Object> data = new HashMap<>();
+
+            data.put("description", "");
+            data.put("img_url", "");
+            data.put("mail", auth.getCurrentUser().getEmail());
+            data.put("time_creation", Timestamp.now());
+            data.put("username", ((TextInputEditText)view.findViewById(R.id.username_tie)).getText());
+
+            user_ref.add(data).addOnSuccessListener(snapshot -> {
+                bottomSheetDialog.dismiss();
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }).addOnFailureListener(e -> {
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            });
+        });
+
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.show();
     }
 }
