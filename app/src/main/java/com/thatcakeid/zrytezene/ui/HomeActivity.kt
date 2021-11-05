1c39f04ee0219b59126a144966e37518c02a55c6
ryenyuku <teamworks1732@gmail.com>
2021-11-05 15:03:28 +0700

    Moved bottom sheet to HomeActivity

--- app/src/main/java/com/thatcakeid/zrytezene/ui/HomeActivity.kt
+++ app/src/main/java/com/thatcakeid/zrytezene/ui/HomeActivity.kt
@@ -2,15 +2,35 @@ package com.thatcakeid.zrytezene.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.thatcakeid.zrytezene.databinding.ActivityHomeBinding
+import com.thatcakeid.zrytezene.databinding.SheetFpuBinding
 
 class HomeActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityHomeBinding.inflate(layoutInflater)
     }
+    private val fpuBinding by lazy { SheetFpuBinding.inflate(layoutInflater) }
+
+    private val bottomSheetBehavior: BottomSheetBehavior<*> by lazy {
+        BottomSheetBehavior
+            .from(fpuBinding.sheetRoot)
+            .also {
+                it.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        fpuBinding.arrowFpuCompactPlayer.setOnClickListener {
+            bottomSheetBehavior.setState(
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED)
+                    BottomSheetBehavior.STATE_EXPANDED
+                else
+                    BottomSheetBehavior.STATE_COLLAPSED
+            )
+        }
     }
 }
--- app/src/main/java/com/thatcakeid/zrytezene/ui/home/HomeFragment.kt
+++ app/src/main/java/com/thatcakeid/zrytezene/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.thatcakeid.zrytezene.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.thatcakeid.zrytezene.R
 import com.thatcakeid.zrytezene.databinding.FragmentHomeBinding
@@ -7,4 +9,8 @@ import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
